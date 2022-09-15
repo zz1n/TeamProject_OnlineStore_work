d2d9cde0c3cbebac5eff29ea181d4e8213c071f1
2022teamproject <113502487+2022teamproject@users.noreply.github.com>
2022-09-15 09:38:17 +0900

    Update CheckController.java

--- TeamProject_OnlineStore/src/main/java/com/teampj/shop/check/CheckController.java
+++ TeamProject_OnlineStore/src/main/java/com/teampj/shop/check/CheckController.java
@@ -24,6 +24,7 @@ public class CheckController {
 	ModelAndView mav = new ModelAndView();
 	JSONParser jp = new JSONParser();
 	JSONObject jo;
+	CheckService ser = sqlSession.getMapper(CheckService.class);
 
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public ModelAndView home(Model model) {
