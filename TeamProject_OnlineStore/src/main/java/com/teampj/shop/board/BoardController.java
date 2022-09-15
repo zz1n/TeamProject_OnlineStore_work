002cd919e6a88c0b001f278a9b5c3b315327aabe
2022teamproject <113502487+2022teamproject@users.noreply.github.com>
2022-09-15 09:38:38 +0900

    Update BoardController.java

--- TeamProject_OnlineStore/src/main/java/com/teampj/shop/board/BoardController.java
+++ TeamProject_OnlineStore/src/main/java/com/teampj/shop/board/BoardController.java
@@ -24,7 +24,7 @@ public class BoardController {
 	ModelAndView mav = new ModelAndView();
 	JSONParser jp = new JSONParser();
 	JSONObject jo;
-	UserService ser = sqlSession.getMapper(UserService.class);
+	BoardService ser = sqlSession.getMapper(BoardService.class);
 
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public ModelAndView home(Model model) {
