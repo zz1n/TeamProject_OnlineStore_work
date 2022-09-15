2a6df61684b1dcddb6c6398d2afb96afccb571b0
2022teamproject <113502487+2022teamproject@users.noreply.github.com>
2022-09-15 12:09:25 +0900

    Update ListController.java

--- TeamProject_OnlineStore/src/main/java/com/teampj/shop/list/ListController.java
+++ TeamProject_OnlineStore/src/main/java/com/teampj/shop/list/ListController.java
@@ -24,7 +24,6 @@ public class ListController {
 	ModelAndView mav = new ModelAndView();
 	JSONParser jp = new JSONParser();
 	JSONObject jo;
-	ListService ser = sqlSession.getMapper(ListService.class);
 
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public ModelAndView home(Model model) {
